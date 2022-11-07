61e99d3d2ecc5437b50695fbcd2a59fdf9f1f369
warghane-monali <monaliwarghane97@gmail.com>
2022-11-07 11:38:00 +0530

    React Netflix website

---
+++ src/Sdata.jsx
@@ -0,0 +1,41 @@
+const Sdata = [
+    {
+      sname : "DARK",
+      imgsrc : "https://i.redd.it/3zcd8oqotej51.jpg",
+      title : "A Netflix Original Series",
+      link : "https://www.netflix.com/in/title/80100172"
+    },
+    {
+      sname : "Narcos",
+      imgsrc : "https://wallpapercave.com/wp/wp1811392.jpg",
+      title : "A Netflix Original Series",
+      link :"https://www.netflix.com/in/title/80025172"
+    },
+    {
+      sname : "Stranger Things",
+      imgsrc : "https://wallpaperaccess.com/full/1087400.jpg",
+      title :"A Netflix Original Series",
+      link : "https://www.netflix.com/in/title/80057281"
+    },
+    {
+      sname : "Squid Game",
+      imgsrc : "https://wallpapercave.com/wp/wp9932802.jpg",
+      title :"A Netflix Original Series",
+      link : "https://www.netflix.com/in/title/81040344"
+    },
+    {
+      sname : "Money Heist",
+      imgsrc : "https://wallpapercave.com/wp/wp4961946.jpg",
+      title :"A Netflix Original Series",
+      link : "https://www.netflix.com/in/title/80192098"
+    },
+    {
+      sname : "The Witcher",
+      imgsrc : "https://wallpapercave.com/wp/wp4423069.jpg",
+      title :"A Netflix Original Series",
+      link : "https://www.netflix.com/in/title/80189685"
+    }
+
+];
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import Card from './Card';
+import Sdata from './Sdata';
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <>
+    <h1 className='heading'>Top 6 Netflix Series</h1>
+    {Sdata.map((val) =>{
+      return(
+        <Card sname = {val.sname} 
+        imgsrc = {val.imgsrc} 
+        title = {val.title}
+        link = {val.link} />
+      )
+    })}
+</>, document.getElementById("root"));
